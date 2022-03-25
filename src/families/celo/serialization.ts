c3fedc57d572653cc9bcc3d2169d17dbd85a8572
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-03-25 14:06:40 +0100

    WIP balances, fix reconciliation

--- src/families/celo/serialization.ts
+++ src/families/celo/serialization.ts
@@ -1,12 +1,33 @@
 // @flow
 import type { CeloResources, CeloResourcesRaw } from "./types";
+import { BigNumber } from "bignumber.js";
 
 export function toCeloResourcesRaw(r: CeloResources): CeloResourcesRaw {
-  const { registrationStatus } = r;
-  return { registrationStatus };
+  const {
+    registrationStatus,
+    lockedBalance,
+    unlockedBalance,
+    unlockingBalance,
+  } = r;
+  return {
+    registrationStatus,
+    lockedBalance: lockedBalance.toString(),
+    unlockedBalance: unlockedBalance.toString(),
+    unlockingBalance: unlockingBalance.toString(),
+  };
 }
 
 export function fromCeloResourcesRaw(r: CeloResourcesRaw): CeloResources {
-  const { registrationStatus } = r;
-  return { registrationStatus };
+  const {
+    registrationStatus,
+    lockedBalance,
+    unlockedBalance,
+    unlockingBalance,
+  } = r;
+  return {
+    registrationStatus,
+    lockedBalance: new BigNumber(lockedBalance),
+    unlockedBalance: new BigNumber(unlockedBalance),
+    unlockingBalance: new BigNumber(unlockingBalance),
+  };
 }
