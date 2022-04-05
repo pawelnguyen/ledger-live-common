3d8136e15bd74070e838ae7e4b25f3e5f1ed90cb
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-04-05 13:08:05 +0100

    Add celo/logic with a method to filter for available pending withdrawals

---
+++ src/families/celo/logic.ts
@@ -0,0 +1,12 @@
+import { Account } from "../../types";
+import { CeloPendingWithdrawal } from "./types";
+
+export const availablePendingWithdrawals = (
+  account: Account
+): CeloPendingWithdrawal[] => {
+  const { pendingWithdrawals } = account.celoResources || {};
+
+  return (pendingWithdrawals || []).filter(
+    (withdrawal) => new Date(withdrawal.time.toNumber() * 1000) < new Date()
+  );
+};
