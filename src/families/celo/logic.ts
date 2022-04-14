6dbc8a028957b3b97ca2b0703095c0415a36e1c0
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-04-14 16:54:29 +0100

    WIP refactoring

--- src/families/celo/logic.ts
+++ src/families/celo/logic.ts
@@ -1,7 +1,9 @@
 import { Account } from "../../types";
-import { CeloPendingWithdrawal } from "./types";
+import { CeloPendingWithdrawal, CeloValidatorGroup } from "./types";
 
 export const PRELOAD_MAX_AGE = 10 * 60 * 1000;
+export const LEDGER_BY_FIGMENT_VALIDATOR_GROUP_ADDRESS =
+  "0x0861a61Bf679A30680510EcC238ee43B82C5e843";
 
 export const availablePendingWithdrawals = (
   account: Account
@@ -12,3 +14,10 @@ export const availablePendingWithdrawals = (
     (withdrawal) => new Date(withdrawal.time.toNumber() * 1000) < new Date()
   );
 };
+
+export const isDefaultValidatorGroupAddress = (address: string): boolean =>
+  address === LEDGER_BY_FIGMENT_VALIDATOR_GROUP_ADDRESS;
+
+export const isDefaultValidatorGroup = (
+  validatorGroup: CeloValidatorGroup
+): boolean => isDefaultValidatorGroupAddress(validatorGroup.address);
