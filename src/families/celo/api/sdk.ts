ac5e2ae61a4e3a4d06fdca0c97f501b050c0e05d
Paweł Nguyen <pawel.nguyen@gmail.com>
2022-04-20 00:48:28 +0200

    WIP pending votes

--- src/families/celo/api/sdk.ts
+++ src/families/celo/api/sdk.ts
@@ -29,3 +29,57 @@ export const getPendingWithdrawals = async (address: string) => {
     .sort((a, b) => a.time.minus(b.time).toNumber());
   return pendingWithdrawalsWithIndexes;
 };
+
+
+// getVotes [
+//   {
+//     group: '0x01b2b83fDf26aFC3Ca7062C35Bc68c8DdE56dB04',
+//     pending: BigNumber { s: 1, e: 16, c: [Array] },
+// active: BigNumber { s: 1, e: 0, c: [Array] },
+// index: 0
+// },
+// {
+//   group: '0x0861a61Bf679A30680510EcC238ee43B82C5e843',
+//     pending: BigNumber { s: 1, e: 16, c: [Array] },
+//   active: BigNumber { s: 1, e: 0, c: [Array] },
+//   index: 1
+// }
+// ]
+
+//TODO: desc, types
+export const getVotes = async (address: string) => {
+  const kit = celoKit();
+
+  const election = await kit.contracts.getElection();
+  const accounts = await kit.contracts.getAccounts();
+  const voteSignerAccount = await accounts.voteSignerToAccount(address);
+  const voter = await election.getVoter(voteSignerAccount)
+
+  return voter.votes;
+};
+
+//DRY
+export const getActivates = async (address: string) => {
+  const kit = celoKit();
+
+  const election = await kit.contracts.getElection();
+  const accounts = await kit.contracts.getAccounts();
+  const voteSignerAccount = await accounts.voteSignerToAccount(address);
+  return await election.activate(voteSignerAccount);
+};
+
+export const getPendingVotes = async (address: string) => {
+  const votes = await getVotes(address);
+  const activates = await getActivates(address);
+
+  return activates.map((activate) => {
+    return votes.find((vote) => vote.group === activate.txo.arguments[0])
+  })
+}
+
+
+// TODO: fetch in sync, to enable/disable vote button
+// const hasPendingVotes = await election.hasPendingVotes(voteSignerAccount);
+// const hasActivatable = await election.hasActivatablePendingVotes(
+//   voteSignerAccount
+// );
